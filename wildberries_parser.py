717c8206013b117259ec443bde72b960e6b18efe
Isfandiyor132 <isfandiyorbaxtiyorov132@gmail.com>
2022-10-09 14:34:49 +0500

    fix buges

--- wildberries_parser.py
+++ wildberries_parser.py
@@ -9,7 +9,7 @@ def wildberries_parser(key_phrase, find_link):
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    browser = webdriver.Chrome(executable_path="chromedriver", chrome_options=chrome_options)
+    browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     position = []
     titles = []
